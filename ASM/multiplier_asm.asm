85535e3a926b2572276903c399b72877a8aa34ca
LisaJacklin <lmjacklin@alaska.edu>
2022-11-26 14:51:51 -0900

    subcircuts
    subcircuits without carry values completed.

--- ASM/multiplier_asm.asm
+++ ASM/multiplier_asm.asm
@@ -1,6 +1,7 @@
  ;11/21/2022
  ;asm bit multiplier
  
+;Now, to begin the full multiplier circuit....using the subcircuits... 
 ret
 
 ;THIS HAS TO BE .DATA! 
@@ -11,6 +12,8 @@ section .data ;note the last value is the least significant!
  	dq 0,0,0,0
  
  
+;now, check to see how I have to implement these sections...
+;adjust the subcircuits to make sure it works with the FULL MULTIPLIER
 Section .data ;this is modifiable values, which should work well for my intermediates!
 PP1:
 	dq 0,0,0,0
@@ -23,6 +26,8 @@ Product:
 EndCarry:
 	dq 0
 
+;ADJUSTMENTS: allowing the input values be changeable to some degree...
+; mostly pertaining to fig1b and fig1c or top and secondrows
 section .text ; this should be executable machine code.
 Adder:
 	;the values for r8 and r9 are given by the the other subcircuits values
@@ -50,6 +55,7 @@ Adder:
 		
 TopRow:;fig1b top row values
  	;first two values
+ 	;ADD IN CARRY LATER
  	mov rax, M ;set to M array adress
  	mov rdi, Q ; set to Q array
  	
@@ -60,14 +66,37 @@ TopRow:;fig1b top row values
 	mov r11, QWORD[rdi+16]
  
 	 cmp r8, r11
-	 ;	je Adder
+	 	je Adder
 	 cmp r9, r10
-	 ;	je Adder
+	 	je Adder
 	;the values for r8 and r9 are given by the the other subcircuits values
 	;so fig1b or fig1c values...
+	;CHECK THAT THERE ISNOT SOMETHING ELSE I NEED TO DO...
+	;HAVE A GUT FEELING IM MISSING SOMETHING
 
 	ret
 	;Return 4219120 (0x4060F0)
 	;this this could be an overload value...
 
-SecondRow:
+SecondRow: ;fig1c, used in the second and last row that gives the output values...
+ 	;ADD CARRY VALUES LATER
+ 	
+ 	mov rax, M
+ 	mov rdi, Q
+ 	mov rcx, Cout ;this is where the carry values come in...so Cout, PP1, and PP2
+ 	
+ 	mov r8, QWORD[rax]
+ 	mov r9, QWORD[rdi]
+ 
+ 	cmp r8,r9
+ 		je A1
+ 		
+ 	cmp r8, rcx ;x1 = M and x2 = cout vallue...or the cc of fig1c
+ 		jmp Adder
+ 	;ADJUST THIS CIRCUIT...
+ 		
+ 	A1: ;I do not want this to be the same as Equal: but atm that is what i'm working with
+ 		mov r11, 1; carry value...
+ 		mov r10, 0; sum value...
+ 		
+	ret
